ae463c5b9e0f47261f3835db4eb08ad51820b91b
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-03-30 10:18:57 -0400

    calendar conversion of date

--- PhotoADay/photoaday.py
+++ PhotoADay/photoaday.py
@@ -1,9 +1,12 @@
 import os
 import cv2
+import calendar
 from PIL import Image
 
 path = os.path.join(os.path.dirname(__file__), "input")
 os.chdir(path)
+
+months = list(calendar.month_name)
 
 num_of_images = len(os.listdir('.'))
 
@@ -29,9 +32,12 @@ def generate_video():
     for image in images:
         image_location = os.path.join(image_folder, image)
         img_data = Image.open(image_location)._getexif()[36867]
-        print(img_data)
+        year = img_data[:4]
+        month = months[int(img_data[5:7])]
+        date = str(int(img_data[8:10]))
+        text = month + " " + date + ", " + year
         frame = cv2.imread(image_location)
-        cv2.putText(frame, img_data, (int(width/2), height-100), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 255, 255), 10)
+        cv2.putText(frame, text, (20, height-120), cv2.FONT_HERSHEY_SIMPLEX, 4.5, (255, 255, 255), 10)
         video.write(frame)
 
     cv2.destroyAllWindows()
