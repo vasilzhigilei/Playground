5d39a64cb191c2809a2e3dfe566be44f0886f3d1
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-04-01 13:42:53 -0400

    comments & descriptions

--- PhotoADay/photoaday.py
+++ PhotoADay/photoaday.py
@@ -13,6 +13,11 @@ days = list(calendar.day_name)
 num_of_images = len(os.listdir('.'))
 
 def generate_video():
+    """
+    Function to generate video using input files in relative folder input
+    outputs to relative file video.avi
+    :return: None
+    """
     image_folder = '.'  # make sure to use your folder
     video_name = '../video.avi'
     os.chdir(path)
@@ -31,20 +36,33 @@ def generate_video():
 
     video = cv2.VideoWriter(video_name, 0, 1, (width, height))
 
+    # loop through all images
     for image in images:
+        # get image location
         image_location = os.path.join(image_folder, image)
+        # get date and time information
         img_data = Image.open(image_location)._getexif()[36867]
+        # get year
         year = img_data[:4]
+        # get month
         month = months[int(img_data[5:7])]
+        # get day of the month
         date = str(int(img_data[8:10]))
+        # convert all to text
         text = month + " " + date + ", " + year
+        # get day of the week
         weekday = datetime.datetime(int(img_data[:4]), int(img_data[5:7]), int(date)).weekday()
+        # read frame from image location
         frame = cv2.imread(image_location)
+        # add two texts, day of the week above and larger
         cv2.putText(frame, days[weekday], (30, height - 190), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 13, cv2.LINE_AA)
         cv2.putText(frame, text, (30, height-70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 10, cv2.LINE_AA)
+        # write video frame
         video.write(frame)
 
+    # cleanup
     cv2.destroyAllWindows()
     video.release()
 
+# Call video generator
 generate_video()
