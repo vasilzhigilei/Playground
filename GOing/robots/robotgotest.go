264e223324f21c0183295658ee916c7bf092efcd
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-08-26 11:30:04 -0400

    GOing commits

--- GOing/robots/robotgotest.go
+++ GOing/robots/robotgotest.go
@@ -1,20 +1,13 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/go-vgo/robotgo"
-)
+import "github.com/go-vgo/robotgo"
 
 func main() {
-	bitmap := robotgo.CaptureScreen(-1000, -1000, 1920+1000, 1080+1000)
-	// use `defer robotgo.FreeBitmap(bit)` to free the bitmap
-	defer robotgo.FreeBitmap(bitmap)
-
-	fmt.Println("...", bitmap)
-
-	fx, fy := robotgo.FindBitmap(bitmap)
-	fmt.Println("FindBitmap------ ", fx, fy)
-
-	robotgo.SaveBitmap(bitmap, "test.png")
+	// you really shouldn't run this.
+	for i := 0; i < 1920; i+=20 {
+		for j:=0; j < 1080; j+=10 {
+			robotgo.MoveMouse(i,j)
+			robotgo.MouseClick("right", true)
+		}
+	}
 }
