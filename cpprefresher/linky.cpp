d5f674c4d1366085c9c05a7189c0af92ca62a67e
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-10-26 19:48:10 -0400

    not much

---
+++ cpprefresher/linky.cpp
@@ -0,0 +1,14 @@
+// a little refresher of things like using linked lists without an IDE
+
+#include <iostream>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+int main(){
+	printf("Hello World!\n");
+
+
+
+}
